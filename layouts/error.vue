e747f288d1c47a4af440e32ddea970897348a7d8
carmine <carminepepiciello@gmail.com>
2021-05-31 13:50:08 +0200

    Nuxtjs - gestioni errori 404 ecc

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<!-- Nuxtjs gestisce automaticamete gli errori, basta creare un layouts di nome error.vue
+ e scrivergli il codice che vogliaomo, in caso di errori lui carica automaticamente questa pagina-->
+<template>
+  <div class="container">
+    <h1>ERRORE</h1>
+    <h2>404</h2>
+  </div>
+</template>
+
+<style>
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
